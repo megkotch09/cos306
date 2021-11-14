810883043449992d20d61a89697901387a6b6c46
megkotch09 <94317957+megkotch09@users.noreply.github.com>
2021-11-14 17:31:24 -0500

    Add files via upload

---
+++ lab0/SimpleCircuit.t.v
@@ -0,0 +1,30 @@
+module SimpleCircuit_test;
+    reg [3:0] counter;
+    wire out;
+
+    SimpleCircuit circuit_under_test(
+        .a(counter[2]),
+        .b(counter[1]),
+        .c(counter[0]),
+        .f(out)
+    );
+
+    always @( * ) begin
+        if (counter[3]) begin
+            $finish;
+        end
+    end
+
+    initial begin  
+        $dumpfile("SimpleCircuit_test.vcd");
+        $dumpvars;
+
+        counter = 4'b0000;
+
+        while (1) begin
+            #1
+            counter = counter + 3'b001;
+        end
+    end
+
+endmodule
